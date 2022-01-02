87d5182dbdc4dff3ab30dd654727fbf5ca0befef
Jaden <shekjaden@gmail.com>
2022-01-02 13:56:57 +0000

    Added repo component to display individual repos

--- src/components/Repo/Repo.vue
+++ src/components/Repo/Repo.vue
@@ -1,13 +1,68 @@
 <template>
-  <div></div>
+  <div class="container">
+    <h4>
+      <a :href="repoData.html_url" target="_blank">{{ repoData.name }}</a>
+    </h4>
+    <p>{{ repoData.description }}</p>
+    <div class="language-container">
+      <div
+        v-for="language in repoData.languages"
+        :key="language"
+        :style="{ width: language.percentage }"
+        class="lang-item"
+        :class="language.name"
+      >
+        {{ language.name }}
+      </div>
+    </div>
+    <div class="watch-stars">
+      <div class="stars">{{ repoData.stargazers_count }}</div>
+      <div class="watchers">{{ repoData.watchers_count }}</div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "Repo",
-  props: {},
+  props: { repoData: { type: Object } },
 };
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+.container {
+  background-color: lightgray;
+  width: 100%;
+  border-radius: 2em;
+  text-align: center;
+}
+
+.language-container {
+  display: flex;
+  width: 80%;
+  overflow: hidden;
+  margin: auto;
+  min-height: 5%;
+  border-radius: 1em;
+}
+
+.lang-item {
+  height: 100%;
+}
+
+.watch-stars {
+  display: flex;
+  justify-content: space-evenly;
+}
+
+.Vue {
+  background-color: green;
+  color: green;
+}
+.JavaScript {
+  background-color: yellow;
+}
+.HTML {
+  background-color: orange;
+}
 </style>
