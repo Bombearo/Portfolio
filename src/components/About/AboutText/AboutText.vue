c1c9b6fd6c5b242ed14fd5ea1a62f4b08dd82e54
Jaden <shekjaden@gmail.com>
2022-01-17 22:42:47 +0000

    Added global styling to h4 and p elements

--- src/components/About/AboutText/AboutText.vue
+++ src/components/About/AboutText/AboutText.vue
@@ -29,27 +29,15 @@ p {
 h4 {
   font-family: montserrat, monospace;
   font-size: clamp(1.5rem, 2.5vw, 2.5rem);
-  letter-spacing: 0.2em;
 
-  font-style: normal;
-  font-weight: 600;
   line-height: 44px;
 
-  /* identical to box height */
-  letter-spacing: 0.15em;
-
-  color: #cd7308;
   text-transform: uppercase;
 }
 
 p {
   font-family: montserrat;
   font-size: clamp(1rem, 1.5vw, 1.5rem);
-  letter-spacing: 0.1rem;
-  font-style: normal;
-  font-weight: normal;
   line-height: 29px;
-
-  color: #000000;
 }
 </style>
