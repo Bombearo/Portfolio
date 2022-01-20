c0d5a2a0933d7e9c5572b8f2d179488f343f4649
Jaden <shekjaden@gmail.com>
2022-01-20 23:20:22 +0000

    Created Github Section

---
+++ src/components/Github/ExperienceText/ExperienceText.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h4>{{title}}</h4>
+        <p>{{text}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:"ExperienceText",
+        props:{
+            title:String,
+            text:String
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
