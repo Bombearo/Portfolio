820582f397d5b8b48433d801b38a4e13b92d6a00
Bombearo <shekjaden@gmail.com>
2023-07-05 15:14:52 +0800

    Created Workitem Template

---
+++ src/components/EducationExperience/WorkItem/WorkItem.vue
@@ -0,0 +1,21 @@
+<template lang="">
+    <div class="workItem">
+        <div class="year"> {{year}} </div>
+        <div class="location"> {{location}}</div>
+    </div>
+</template>
+<script>
+export default {
+    name:"WorkItem",
+    props: {
+    year: { type: String },
+    location: { type: String },
+  },
+}
+</script>
+<style lang="">
+.workItem{
+    border-radius: 15px;
+    border: 3px solid #657F56;
+}
+</style>
