4a464d4fc150228b2d788b496945b40b9f583c1a
Bombearo <shekjaden@gmail.com>
2023-07-10 23:33:59 +0800

    Created a welcome section

---
+++ src/components/WelcomeContainer/WelcomeContainer.vue
@@ -0,0 +1,35 @@
+<template>
+    {{ path }}
+    <SectionContainer>
+      <template v-slot:content>
+        <WelcomeText />
+      </template>
+      <template v-slot:media>
+        <WelcomeGraphic :svgPath="svgPath"/>
+      </template>
+    </SectionContainer>
+  </template>
+<script>
+import SectionContainer from "../SectionContainer/SectionContainer.vue"
+import WelcomeText from "./WelcomeText/WelcomeText.vue"
+import WelcomeGraphic from "./WelcomeGraphic/WelcomeGraphic.vue"
+
+let publicPath = process.env.BASE_URL;
+let svgPath =  `${publicPath}SVGs/HTML.svg`
+export default {
+    name:"WelcomeContainer",
+    components:{
+        SectionContainer,
+        WelcomeText,
+        WelcomeGraphic
+    },
+    data(){
+        return{
+            svgPath:svgPath
+        }
+    }
+}
+</script>
+<style lang="">
+    
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <WelcomeContainer />
     <AboutContainer />
     <MidCard />
     <GithubDesc />
@@ -13,8 +14,9 @@
 import AboutContainer from "@/components/About/AboutContainer/AboutContainer.vue";
 import GithubDesc from "@/components/Github/GithubDesc/GithubDesc.vue";
 import MidCard from "@/components/MidCard/MidCard.vue";
-import ContactContainer from "../components/ContactContainer/ContactContainer.vue";
-import EducationExperienceContainer from "../components/EducationExperience/EducationExperienceContainer.vue";
+import ContactContainer from "@/components/ContactContainer/ContactContainer.vue";
+import EducationExperienceContainer from "@/components/EducationExperience/EducationExperienceContainer.vue";
+import WelcomeContainer from "@/components/WelcomeContainer/WelcomeContainer.vue";
 
 export default {
   name: "HomePage",
@@ -24,6 +26,7 @@ export default {
     MidCard,
     ContactContainer,
     EducationExperienceContainer,
+    WelcomeContainer
   },
 };
 </script>
